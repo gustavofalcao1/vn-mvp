76702ac040c45972a535af11299a1a48b31a2141
Gustavo Falcão <gusta@monkey.lan>
2025-05-07 14:02:35 +0100

    Fix and refactore structure for vercel deploy v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
